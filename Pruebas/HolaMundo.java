9468f32d135df65aedf7eacdd0ed8b43ebdfdd0b
Juan Pablo Jimenez Franco <juanp.jimenezfranco@gmail.com>
2023-04-18 13:39:35 +0200

    Prueba de HolaMundo

---
+++ Pruebas/HolaMundo.java
@@ -0,0 +1,13 @@
+package Pruebas;
+
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("-----------");
+        System.out.println("Hola Mundo");
+        System.out.println("-----------");
+    }
+}
